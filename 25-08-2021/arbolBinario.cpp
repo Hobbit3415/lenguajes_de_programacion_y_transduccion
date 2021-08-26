f26db000d3c07b8d90dff03110f2c6c2119ca21e
Hobbit3415 <emmanuel.mora3415@gnail.com>
2021-08-26 12:50:11 -0500

    Se agregaron metodos para mostrar el arbol en inOrden, PreOrden y PostOrden

--- 25-08-2021/arbolBinario.cpp
+++ 25-08-2021/arbolBinario.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 bool seguir = true;
 
-struct NodoA{
+struct NodoA
+{
   int dato;
   NodoA *der;
   NodoA *izq;
@@ -10,75 +11,157 @@ struct NodoA{
 
 NodoA *arbol = NULL;
 
-NodoA *crearNodos(int d){
+NodoA *crearNodos(int d)
+{
   NodoA *nnodo = new NodoA();
-  nnodo -> dato = d;
-  nnodo -> der=NULL; 
-  nnodo -> izq=NULL;
+  nnodo->dato = d;
+  nnodo->der = NULL;
+  nnodo->izq = NULL;
   return nnodo;
 }
 
-void insertar(NodoA *&arbol, int d){
-  if(arbol==NULL){
+void insertar(NodoA *&arbol, int d)
+{
+  if (arbol == NULL)
+  {
     NodoA *nnodo = crearNodos(d);
     arbol = nnodo;
-  }else{
-    int valorRaiz = arbol -> dato;
-    if(d < valorRaiz){
-      insertar(arbol -> izq, d);
-    }else{
-      insertar(arbol -> der, d);
+  }
+  else
+  {
+    int valorRaiz = arbol->dato;
+    if (d < valorRaiz)
+    {
+      insertar(arbol->izq, d);
+    }
+    else
+    {
+      insertar(arbol->der, d);
     }
   }
 }
 
-void mostrar(NodoA *arbol, int cont){
-  if(arbol==NULL){
+void mostrar(NodoA *arbol, int cont)
+{
+  if (arbol == NULL)
+  {
     //system("pause");
     return;
-  }else{
-    mostrar(arbol ->der, cont+1);
-    for(int i =0; i< cont; i++){
-      cout<<"\t";
+  }
+  else
+  {
+    mostrar(arbol->der, cont + 1);
+    for (int i = 0; i < cont; i++)
+    {
+      cout << "\t";
     }
-    cout<<arbol->dato<<endl;
-    mostrar(arbol ->izq, cont+1);
+    cout << arbol->dato << endl;
+    mostrar(arbol->izq, cont + 1);
   }
 }
 
-int main(){
-  int opcion, contador=0;
+void inOrden(NodoA *arbol){
+  if (arbol == NULL)
+  {
+    //system("pause");
+    return;
+  }
+  else
+  {
+    inOrden(arbol->izq);
+    cout << arbol->dato<< "\t";
+    inOrden(arbol->der);
+  }
+}
+
+void preOrden(NodoA *arbol){
+  if (arbol == NULL)
+  {
+    //system("pause");
+    return;
+  }
+  else
+  {
+    cout << arbol->dato<< "\t";
+    preOrden(arbol->izq);
+    preOrden(arbol->der);
+  }
+}
+
+void postOrden(NodoA *arbol){
+  if (arbol == NULL)
+  {
+    //system("pause");
+    return;
+  }
+  else
+  {
+    postOrden(arbol->izq);
+    postOrden(arbol->der);
+    cout << arbol->dato<< "\t";
+  }
+}
+
+int main()
+{
+  int opcion, contador = 0;
   int dato;
 
-  do{
+  do
+  {
     system("cls");
-    cout<<"Operaciones Arbol Binario"<<endl;
-    cout<<"1. Insertar nodo\n2. Mostrar\n3. Salir"<<endl;
-    cout<<"Ingrese una opcion..."<<endl;
-    cin>>opcion;
-    switch(opcion){
-      case 1:
-        system("cls");
-        cout<<"Ingrese el elemento que va a almacenar en el arbol: "<<endl;
-        cin>>dato;
-        insertar(arbol, dato);
-        break;
+    cout << "Operaciones Arbol Binario" << endl;
+    cout << "1. Insertar nodo\n2. Mostrar\n3. Mostrar In Orden" << endl;
+    cout << "4. Mostrar Pre Orden\n5. Mostrar Post Orden\n6. Salir" << endl;
+    cout << "Ingrese una opcion..." << endl;
+    cin >> opcion;
+    switch (opcion)
+    {
+    case 1:
+      system("cls");
+      cout << "Ingrese el elemento que va a almacenar en el arbol: " << endl;
+      cin >> dato;
+      insertar(arbol, dato);
+      break;
 
-      case 2:
-        system("cls");
-        cout<<"Mostrar Arbol..."<<endl;
-        mostrar(arbol, contador);
-        system("pause");
-        break;
+    case 2:
+      system("cls");
+      cout << "Mostrar Arbol..." << endl;
+      mostrar(arbol, contador);
+      system("pause");
+      break;
 
-      case 3:
-        seguir = false;
-        break;
-      
-      default:
-        cout<<"Opcion no valida..."<<endl;
-        break;
+    case 3:
+      system("cls");
+      cout << "** In Orden **" << endl;
+      inOrden(arbol);
+      cout<<endl;
+      system("pause");
+      break;
+
+    case 4:
+      system("cls");
+      cout << "** Pre Orden **" << endl;
+      preOrden(arbol);
+      cout<<endl;
+      system("pause");
+      break;
+
+    case 5:
+      system("cls");
+      cout << "** Post Orden **" << endl;
+      postOrden(arbol);
+      cout<<endl;
+      system("pause");
+      break;
+
+    case 6:
+      seguir = false;
+      break;
+
+    default:
+      cout << "Opcion no valida..." << endl;
+      break;
     }
-  }while(seguir == true);
-
+  } while (seguir == true);
 }
